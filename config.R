a9125d04a78ddc85b9dce3094389862b7bd0401b
NathanDeMaria <naterstd@gmail.com>
2015-06-07 22:56:43 -0500

    Add check for file existing

--- config.R
+++ config.R
@@ -1,6 +1,9 @@
 library(XML)
 
 get_config <- function(key, location='config.xml') {
+	if (!file.exists(location)) {
+		stop(sprintf("No config file found at %s", location))
+	}
 	doc <- xmlParse(location)
 	xpath_query <-  sprintf("//config/item[@key='%s']", key)
 	matches <- getNodeSet(doc, xpath_query)
